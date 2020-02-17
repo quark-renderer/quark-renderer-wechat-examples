48984a12db02bd95c344795b7e5f14326531e60b
damoqiongqiu <damoqiongqiu@126.com>
2020-02-17 20:09:02 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,30 @@
+const app = getApp()
+Page({
+  data: {
+  },
+  toDemo1(){
+    wx.navigateTo({
+      url: '../demo-1/demo-1'
+    });
+  },
+  toDemo2(){
+    wx.navigateTo({
+      url: '../demo-2/demo-2'
+    });
+  },
+  toDemo3(){
+    wx.navigateTo({
+      url: '../demo-3/demo-3'
+    });
+  },
+  toDemo4(){
+    wx.navigateTo({
+      url: '../demo-4/demo-4'
+    });
+  },
+  toDemo5(){
+    wx.navigateTo({
+      url: '../demo-5/demo-5'
+    });
+  }
+})
